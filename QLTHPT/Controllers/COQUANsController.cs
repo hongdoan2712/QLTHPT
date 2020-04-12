d5f18fbc353cc2ad14891e876f850899a23a58ce
NhanNguyen1152 <46920791+NhanNguyen1152@users.noreply.github.com>
2020-04-12 13:40:24 +0700

    Nhân commit tạo mã tự động các bảng baclong, vanbang, ngachluong, cơuqaan

--- QLTHPT/Controllers/COQUANsController.cs
+++ QLTHPT/Controllers/COQUANsController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using QLTHPT.App_Start;
 using QLTHPT.Models;
 
 namespace QLTHPT.Controllers
@@ -38,7 +39,10 @@ namespace QLTHPT.Controllers
         // GET: COQUANs/Create
         public ActionResult Create()
         {
-            return View();
+            COQUAN obj = new COQUAN();
+            obj.CQ_MA = CreateID.CreateID_ByteText();
+            return View(obj);
+           
         }
 
         // POST: COQUANs/Create
